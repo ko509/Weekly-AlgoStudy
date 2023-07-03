f065bbd63574be6bec4e7395da2fd559314bff71
freemjstudio <mjwoo0@naver.com>
2023-07-04 08:41:56 +0900

    0704

--- week1/우민지/[2075] N번째 큰수.py
+++ week1/우민지/[2075] N번째 큰수.py
@@ -1,7 +1,18 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
 N = int(input())
 
-array = []
+queue = [] # 최소 힙
 for i in range(N):
-    array.extend(list(map(int, input().split())))
-array.sort(reverse=True)
-print(array[N-1])
+    data = list(map(int, input().split()))
+    for x in data:
+        if len(queue) < N:
+            heapq.heappush(queue, x)
+        else:
+            if queue[0] < x:
+                heapq.heappop(queue) # 가장 작은 수
+                heapq.heappush(queue, x)
+print(queue[0])
+
