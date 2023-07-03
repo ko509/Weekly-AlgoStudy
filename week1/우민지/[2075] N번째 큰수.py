61998837843f47020b239be7cfecf87ab2561f34
freemjstudio <mjwoo0@naver.com>
2023-07-03 22:55:37 +0900

    0703

--- week1/우민지/[2075] N번째 큰수.py
+++ week1/우민지/[2075] N번째 큰수.py
@@ -2,4 +2,6 @@ N = int(input())
 
 array = []
 for i in range(N):
-    array.append(list(map(int, input().split())))
+    array.extend(list(map(int, input().split())))
+array.sort(reverse=True)
+print(array[N-1])
