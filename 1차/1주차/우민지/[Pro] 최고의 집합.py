2934272f91758e0bd3d92c40a4811ff85949c8f3
freemjstudio <mjwoo0@naver.com>
2023-07-04 21:23:28 +0900

    0704

--- 1차/1주차/우민지/[Pro] 최고의 집합.py
+++ 1차/1주차/우민지/[Pro] 최고의 집합.py
@@ -1,17 +1,20 @@
 def solution(n, s):
     answer = []
-    if n > s:  # 나눌 수 없는 경우
+
+    if n > s:
         return [-1]
-    # 원소들 끼리의 차이가 가장 적도록 만들어야 함
-    x = s // n
+    x = s // n  # 가장 큰 곱을 위한 element
+
+    left = s - x * n
     for i in range(n):
         answer.append(x)
-        s -= x
-    if s > 0:
-        for i in range(n):
-            s -= 1
-            answer[i] += 1
-            if s == 0:
-                break
+    if left == 0:
+        return answer
+    else:
+        idx = 0
+        while left:
+            answer[idx] += 1
+            left -= 1
+            idx += 1
 
-    return sorted(answer)
+    return sorted(answer)  # 정렬
