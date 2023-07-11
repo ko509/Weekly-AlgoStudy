4fe900aade1b4de6f992ab9fbf5e180b8c111e85
NaCl823400 <50520329+NaCl823400@users.noreply.github.com>
2023-07-12 01:52:01 +0900

    [14499] 주사위 굴리기

--- 1차/2주차/정병현/[14499] 주사위 굴리기.cpp
+++ 1차/2주차/정병현/[14499] 주사위 굴리기.cpp
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 using uint=unsigned int;
-void rn(vector<int>& v);
-void rs(vector<int>& v);
-void rw(vector<int>& v);
-void re(vector<int>& v);
+void rn(vector<int>& v); // rotate north
+void rs(vector<int>& v); // rotate south
+void rw(vector<int>& v); // rotate west
+void re(vector<int>& v); // rotate east
 int main(){
 	cin.tie(0)->sync_with_stdio(0);
-	int n, m, x, y, q, t, dy[5] = {0, 1, -1, 0, 0}, dx[5] = {0, 0, 0, -1, 1};
+	int n, m, x, y, q, t, dy[5] = {0, 1, -1, 0, 0}, dx[5] = {0, 0, 0, -1, 1}; // different of positions
 	vector<vector<int>> board;
-	vector<int> dice(6);
-	void (*rotate[5])(vector<int>& v) = {0, re, rw, rn, rs};
+	vector<int> dice(6); // resize to size 6 and make each elemet 0, [0] : floorside, [2] : upperside
+	void (*rotate[5])(vector<int>& v) = {0, re, rw, rn, rs}; // rotation array
 	cin >> n >> m >> x >> y >> q;
 	board.resize(n);
 	for(auto&i : board)
@@ -26,20 +26,20 @@ int main(){
 		if(tx >= n || ty >= m)
 			continue;
 		rotate[t](dice);
-		if(board[tx][ty] == 0)
+		if(board[tx][ty] == 0) // first case, if a number on the board is zero
 			board[tx][ty] = dice[0];
-		else{
+		else{ // else case, if a number on the board is not a zeryo
 			dice[0] = board[tx][ty];
 			board[tx][ty] = 0;
 		}
-		x = tx; y = ty;
-		cout << dice[2];
+		x = tx; y = ty; // position update
+		cout << dice[2]; // print upper side of dice
 //		for(auto i : dice)
 //			cout << i << " ";
 		cout << "\n";
 	}
 }
-
+//implementation of rotation operations
 void rn(vector<int>& v){
 	int t = v[0];
 	v[0] = v[1];
