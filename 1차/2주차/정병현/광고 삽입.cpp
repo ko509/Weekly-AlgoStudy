7618d56e2ddb5f9fa72d12923851fc48b0645d65
NaCl823400 <50520329+NaCl823400@users.noreply.github.com>
2023-07-12 01:54:33 +0900

    [72414]광고 삽입

--- 1차/2주차/정병현/광고 삽입.cpp
+++ 1차/2주차/정병현/광고 삽입.cpp
@@ -6,22 +6,25 @@ string solution(string play_time, string adv_time, vector<string> logs) {
     int n = logs.size();
     char c;
     int h, m, s, ptime, atime;
-    stringstream ssin;
+    stringstream ssin; // use sstream for easy parsing
     vector<int> times, watching;
     string ans = "";
+    ///// get playtime
     ssin.str(play_time);
     ssin >> h >> c >> m >> c >> s;
     ssin.clear();
     ptime = 3600*h + 60*m + s;
+    
     times.resize(ptime+1);
     watching.resize(ptime+1);
+    ///// get advertising time
     ssin.str(adv_time);
     ssin >> h >> c >> m >> c >> s;
     ssin.clear();
     atime = 3600*h + 60*m + s;
     // cout << "ptime : " << ptime << "\n";
     // cout << "atime : " << atime << "\n";
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++){ // make differentiation array of watching people
         ssin.str(logs[i]);
         ssin >> h >> c >> m >> c >> s >> c;
         times[3600*h + 60*m + s]++;
