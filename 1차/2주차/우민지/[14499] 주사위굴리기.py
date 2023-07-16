5a285651d8e10c277676e8be09e0de5ed2f688af
freemjstudio <mjwoo0@naver.com>
2023-07-16 14:29:18 +0900

    0716

--- 1차/2주차/우민지/[14499] 주사위굴리기.py
+++ 1차/2주차/우민지/[14499] 주사위굴리기.py
@@ -1,1 +1,11 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/72414
+# https://www.acmicpc.net/problem/14499
+
+n, m, x, y, k = map(int, input().split())
+# 지도에 쓰여 있는 수
+board = []
+for _ in range(n):
+    board.append(list(map(int, input().split())))
+# 명령
+command = list(map(int, input().split()))
+
+
