55548cafd03bb7175e2f37e7af9eafc087bb6ff6
freemjstudio <mjwoo0@naver.com>
2023-08-05 08:35:27 +0900

    [1644] 소수의 연속합

--- 1차/5주차/우민지/[1644] 소수의 연속합.py
+++ 1차/5주차/우민지/[1644] 소수의 연속합.py
@@ -2,11 +2,27 @@ N = int(input())
 check = [True] * (N+1) # 1 ~ N
 sosu = [] # 소수 list
 
+answer = 0
 # 1. 소수 구하기
 for i in range(2, N+1):
     if check[i]:
         sosu.append(i)
-        for j in range(i, N+1, i): # i 의 배수 제거하기
-            if check[j]:
-                check[j] = False
+        for j in range(2*i, N+1, i): # i 의 배수 제거하기
+            check[j] = False
 
+print(sosu)
+
+# 2. 경우의 수 구하기 - two pointer
+start = 0
+end = 0
+while end <= len(sosu):
+    total = sum(sosu[start:end])
+    if total == N:
+        answer += 1
+        end += 1
+    elif total < N:
+        end += 1
+    else:
+        start += 1 # 값이 큰 경우 start pointer 를 옮겨서 값을 줄인다
+
+print(answer)
