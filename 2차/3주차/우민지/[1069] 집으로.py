d46f82aa6098ac5be0c466d9c7a9f928b61f049d
freemjstudio <mjwoo0@naver.com>
2023-09-02 00:00:49 +0900

    1069

--- 2차/3주차/우민지/[1069] 집으로.py
+++ 2차/3주차/우민지/[1069] 집으로.py
@@ -1,12 +1,15 @@
 X,Y,D,T = map(int, input().split())
+# T초에 D칸 만큼 이동함
 
-min_value = int(1e9)
+answer = int(1e9) # 시간
 
-# 1. 1칸 이동 -> X+1, X-1, Y+1, Y-1
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-# 2. JUMP T초에 D만큼
+# 대각선 거리를 구해야 함 !
+distance = (X**2 + Y**2) ** 0.5 # 그냥 걷기
 
-
-
-print(min_value)
+if distance >= D: # 1칸씩 이동
+    answer = min(distance,
+                 T * (distance // D +1),
+                 T * (distance //D) + distance % D)
+else: # 걷기, 점프한다음에 남은거리 걷기, 점프
+    answer = min(distance, T + D-distance , 2*T)
+print(answer)
