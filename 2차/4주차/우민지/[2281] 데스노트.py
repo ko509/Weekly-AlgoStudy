88e6ef4bd317de8972daf3b682916b3c9259f3c7
freemjstudio <mjwoo0@naver.com>
2023-09-10 19:47:32 +0900

    deathnote

--- 2차/4주차/우민지/[2281] 데스노트.py
+++ 2차/4주차/우민지/[2281] 데스노트.py
@@ -1,6 +1,9 @@
 # https://www.acmicpc.net/problem/2281
 
 n, m = map(int, input().split())
-name = []
+name = [] # 7, 4, 2, 3, 2, 5, 1, 12, 7, 5, 6
+answer = int(1e9) # 남게 되는 칸의 수의 제곱의 합이 최소가 되도록 한다
 for _ in range(n):
     name.append(int(input()))
+
+print(answer)
