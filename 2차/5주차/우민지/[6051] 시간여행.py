fca3d1e5422c9e03b6c9a9e65a3d198effcf323c
freemjstudio <mjwoo0@naver.com>
2023-09-17 18:01:18 +0900

    6051 시간여행

--- 2차/5주차/우민지/[6051] 시간여행.py
+++ 2차/5주차/우민지/[6051] 시간여행.py
@@ -2,28 +2,32 @@
 
 N = int(input())
 query = []
-log_stack = [[-1]] # 모든 기록
+log = [[] for _ in range(N+2)]# 모든 기록
 stack = [] # 가장 최근에 푼 문제 = 스택 맨 위
+
 for idx in range(N):
     data = list(input().split())
-    if len(data) == 1:  # s 연산
-        if len(stack) > 0:
-            top = stack[-1]
+    command = data[0]
+    top = 0
+    # print(stack)
+    if command == 'a': # add
+        k = int(data[1])
+        stack.append(k)
+        log[idx+1].extend(stack)
+        top = stack[-1]
+    elif command == 's': # delete
+        if len(stack) > 1:
             stack.pop()
+            log[idx+1].extend(stack)
+            top = stack[-1]
         else:
             print(-1)
-            continue # 다음 query 로 넘기기
-    else:  # a, t 연산
-        command, k = data[0], int(data[1])
-        if command == 'a':
-            stack.append(k)
-            top = stack[-1]
-        else: # command == 't'
-            stack = log_stack[k-1]
-            top = stack[-1]
-        print(top)
-    log_stack.append(stack)
+            continue
+    else: # t
+        k = int(data[1])
+        tmp = log[k-1]
+        log[idx+1].extend(tmp)
+        stack = tmp
+        top = tmp[-1]
 
-
-
-
+    print(top)
