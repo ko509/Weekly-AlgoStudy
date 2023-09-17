02526b180e1f35e685d83f9ee625fec2f1377e61
freemjstudio <mjwoo0@naver.com>
2023-09-17 20:21:52 +0900

    상담원 인원

--- 2차/5주차/우민지/[Pro] 상담원인원.py
+++ 2차/5주차/우민지/[Pro] 상담원인원.py
@@ -1,1 +1,11 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/214288
+
+def solution(k, n, reqs):
+    answer = 0  # 기다린 시간의 최솟값
+    types = [{i: []} for i in range(1, k + 1)]  # 유형별로 분류하기
+
+    for i in range(len(reqs)):
+        start, dur, t = reqs[i]
+        types[t - 1].append([start, dur])
+
+    return answer
