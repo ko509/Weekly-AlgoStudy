e9bd47ac1cd581fba6aa0dbcdfd9bdee78fe7013
freemjstudio <mjwoo0@naver.com>
2023-09-17 22:46:55 +0900

    상담원 인원

--- 2차/5주차/우민지/[Pro] 상담원인원.py
+++ 2차/5주차/우민지/[Pro] 상담원인원.py
@@ -1,12 +1,12 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/214288
-from heapq import heappush, heappop
+import heapq
 from itertools import combinations_with_replacement
+from collections import defaultdict
 
 
 def solution(k, n, reqs):
     answer = int(1e9)  # 기다린 시간의 최솟값
     # types = [{i: []} for i in range(1, k+1)] # 유형별로 분류하기
-    type_list = [{i: []} for i in range(k)]  # type 별 priority queue
+    type_list = defaultdict(list)  # type 별 priority queue
 
     # 1.  n-k 명 멘토 할당하는 모든 경우의 수 구하기
     comb = combinations_with_replacement([i for i in range(k)], r=n - k)  # nCr
@@ -26,13 +26,21 @@ def solution(k, n, reqs):
         # type 별로 처리하기
         for i in range(k):
             t_list = type_list[i]
+            mento_list = []  # priority queue
+
+            for _ in range(n):
+                heapq.heappush(mento_list, 0)
+
             for start, end in t_list:
+                mento = heapq.heappop(mento_list)  # mento가 있는 시간
                 if mento <= start:  # mento 가 available 한 경우
-
+                    heapq.heappush(mento_list, end)
                 else:  # mento 기다려야 하는 경우
+                    wait = mento - start
                     total += wait
+                    heapq.heappush(mento_list, end + wait)
 
-                    # 최솟값 아닌 경우 pass
+            # 최솟값 아닌 경우 pass
             if total > answer:
                 break
 
