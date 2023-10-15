3476313003c6de518f9fbec9cc5b962ab1e4d1b2
sumeen99 <70104259+sumeen99@users.noreply.github.com>
2023-10-16 03:25:52 +0900

    수정

--- 3차/3주차/이수민/9446.py
+++ 3차/3주차/이수민/9446.py
@@ -1,20 +1,24 @@
 import sys
 sys.setrecursionlimit(111111)
 
-def find(start,n):
-    global tmp 
+def find(n):
     global total
+    global Circle
     
-    if visited[n] == 1:
-        if n == start:
-            tmp = True
-        else:
-            tmp = False
-        return
     Circle.append(n)
     visited[n] = 1
+    next = S[n]-1
     
-    find(start,S[n]-1)
+    if visited[next] == 1:
+        if next in Circle:
+            Circle = Circle[Circle.index(next):]
+        else :
+            Circle = []
+        return
+    
+    
+    
+    find(S[n]-1)
 
 
 T = int(input())
@@ -22,22 +26,16 @@ for _ in range(T):
     n = int(input())
     S = list(map(int,input().split()))
     
-    tmp = 0
     total = 0
     visited = [0 for _ in range(n)]
     for i in range(n):
-        if i == S[i]-1:
-            visited[i] = 1
-            total +=1
-            continue 
         if visited[i] == 1:
             continue
         
         Circle = []
-        find(i,i)
-        if tmp :
-            total += len(Circle)
+        find(i)
+        total += len(Circle)
             
+            
+             
     print(n-total)
-    
-        
