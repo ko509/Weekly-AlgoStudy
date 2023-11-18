503c5750b7d831d8cc4b95c5e1bc8e87ca9478dc
freemjstudio <mjwoo0@naver.com>
2023-11-18 23:29:21 +0900

    [Programmers] 코딩 테스트 공부

---
+++ 4차/2주차/우민지/[kakao] 코딩테스트 공부.py
@@ -0,0 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/118668
+
+def solution(alp, cop, problems):
+    INF = int(1e9)
+    answer = INF  # 최단 시간
+
+    visited = []  # 푼 문제 체크
+
+    return answer
