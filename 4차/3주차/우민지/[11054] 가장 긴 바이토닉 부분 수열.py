5d7a75581974272ffb1b946c36b0b28d8e641c35
freemjstudio <mjwoo0@naver.com>
2023-11-26 09:09:33 +0900

    [Programmers] 수식 최대화

--- 4차/3주차/우민지/[11054] 가장 긴 바이토닉 부분 수열.py
+++ 4차/3주차/우민지/[11054] 가장 긴 바이토닉 부분 수열.py
@@ -10,7 +10,7 @@ for i in range(N):
         if data[i] > data[j]:
             dp_up[i] = max(dp_up[i], dp_up[j] + 1)
 # 2. 감소하는 부분 수열 구하기
-dp_down = [1] * (N+1)
+dp_down = [1] * N
 
 for i in range(N-1, -1, -1):
     for j in range(N-1, i, -1):
@@ -20,6 +20,13 @@ for i in range(N-1, -1, -1):
 # 3. 합쳐서 최대 길이 구하기
 dp = [0] * N
 for i in range(N):
-    dp[i] = dp_up[i] + dp_down[i] -1
+    dp[i] = dp_up[i] + dp_down[i] -1 # 중복 원소 삭제
+
+print("dp_up")
+print(dp_up)
+print("dp_down")
+print(dp_down)
+print("DP")
+print(dp)
 
 print(max(dp))
